c44ec1e88279fc6bf037f8790fff254d3739abd7
esk98 <87301102+esk98@users.noreply.github.com>
2022-10-04 21:09:07 +0700

    3 files modified

--- components/form.js
+++ components/form.js
@@ -1,7 +1,27 @@
-
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import * as Yup from "yup";
 
 export default function Form() {
   return (
-    <h1>Form for contact here</h1>
+    <Formik
+          initialValues={{
+            name: "",
+            email: "",
+            message: "",
+          }}
+          validationSchema={Yup.object({
+            message: Yup.string()
+              .max(250, "Must be 100 characters or less")
+              .required("Required"),
+            email: Yup.string()
+              .email("Invalid email address")
+              .required("Required"),
+          })}
+          onSubmit={(values, { setSubmitting }) => {
+            sendForm(values.name, values.email, values.message);
+            setSubmitting(false);
+          }}
+        >
+      </Formik>
   );
 }
